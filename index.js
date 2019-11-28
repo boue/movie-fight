bef8716c4036ef1d8151c690e87d6ca0df37c94e
boue <boue.germain@gmail.com>
2019-11-28 10:17:58 -0800

    Refactored to have a debounce function, that has a default delay of 500, but we can also pass in a delay of however much delay we want

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const fetchData = async (searchTerm) => {
+const fetchData = async searchTerm => {
     const response = await axios.get('http://www.omdbapi.com/', {
-        //sticks these as query strings    
+        //sticks these as query strings
         params: {
             apikey: '',
             s: searchTerm
         }
-    })
-    console.log(response.data)
-}
+    });
+    console.log(response.data);
+};
 
-const input = document.querySelector('input')
-let timeoutId;
-const onInput = (event) => {
-    if (timeoutId) {
-        clearTimeout(timeoutId)
-    }
-    timeoutId = setTimeout(() => {
-        fetchData(event.target.value)
-    }, 1000)
-}
+const input = document.querySelector('input');
 
-input.addEventListener('input', onInput)
+const debounce = (func, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+};
+
+const onInput = event => {
+    fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 3000));
