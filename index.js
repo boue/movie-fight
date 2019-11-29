63e4e35842538a1e0a69f03f64b0e9466dd42bf0
boue <boue.germain@gmail.com>
2019-11-29 10:28:58 -0800

    Add a summary div container which renders specific movie information on the page once user makes their selection from dropdown options

--- index.js
+++ index.js
@@ -49,6 +49,12 @@ const onInput = async event => {
             ${movie.Title}
         `
 
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active')
+            input.value = movie.Title
+            onMovieSelect(movie)
+        })
+
         resultsWrapper.appendChild(option)
     }
 };
@@ -61,3 +67,33 @@ document.addEventListener('click', (e) => {
         dropdown.classList.remove('is-active')
     }
 })
+
+const onMovieSelect = async (movie) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        //sticks these as query strings
+        params: {
+            apikey: '33c82e67',
+            i: movie.imdbID
+        }
+    });
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = movieDetail => {
+    return `
+      <article class="media">
+        <figure class="media-left">
+          <p class="image">
+            <img src="${movieDetail.Poster}" />
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+          </div>
+        </div>
+      </article>
+    `;
+};
