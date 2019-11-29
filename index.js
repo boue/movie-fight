d783bf8cf87c24eaeef47cd898ae2c50c5db3b06
boue <boue.germain@gmail.com>
2019-11-29 09:52:23 -0800

    Handle bug where dropdown stays empty when there is no search results instead of not appearing

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ const resultsWrapper = document.querySelector('.results')
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
 
+    if (!movies.length) {
+        dropdown.classList.remove('is-active')
+        return
+    }
+
     resultsWrapper.innerHTML = ''
     dropdown.classList.add('is-active')
     for (let movie of movies) {
@@ -49,7 +54,7 @@ const onInput = async event => {
 };
 
 
-input.addEventListener('input', debounce(onInput, 2000));
+input.addEventListener('input', debounce(onInput, 1000));
 
 document.addEventListener('click', (e) => {
     if (!root.contains(e.target)) {
