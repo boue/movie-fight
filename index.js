78786efa85a2ad108c9453e8a74476c6e5daa478
boue <boue.germain@gmail.com>
2019-12-02 18:23:36 -0800

    Add CSS for the columns so results show up in half of the screen. Ensure that onOptionSelect is specific to an element within the createAutoComplete config file

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-createAutoComplete({
-    root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
     renderOption: (movie) => {
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
         return `
             <img src="${imgSrc}" />
             ${movie.Title} (${movie.Year})
         `
-    },
-    onOptionSelect: (movie) => {
-        onMovieSelect(movie)
     },
     inputValue: (movie) => {
         return movie.Title
@@ -28,9 +24,27 @@ createAutoComplete({
 
         return response.data.Search
     }
+}
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete'),
+    onOptionSelect: (movie) => {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#left-summary'))
+    }
 })
 
-const onMovieSelect = async (movie) => {
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
+    onOptionSelect: (movie) => {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#right-summary'))
+    }
+})
+
+const onMovieSelect = async (movie, summaryElement) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         //sticks these as query strings
         params: {
@@ -38,7 +52,7 @@ const onMovieSelect = async (movie) => {
             i: movie.imdbID
         }
     });
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+    summaryElement.innerHTML = movieTemplate(response.data)
 }
 
 const movieTemplate = movieDetail => {
